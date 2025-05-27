fb3acd939646d1592021602bec8edd3da39c1282
vinaybv <vinay.bv@accionlabs.com>
2025-05-27 19:53:21 +0530

    actions

--- socket-script.js
+++ socket-script.js
@@ -1,1 +1,13 @@
-console.log("passs");
+function main() {
+  console.log("main");
+  let i = 0;
+  const interval = setInterval(() => {
+    i++;
+    console.log("call", i);
+    if (i === 50) {
+      clearInterval(interval);
+      process.exit();
+    }
+  }, 1000);
+}
+main();
